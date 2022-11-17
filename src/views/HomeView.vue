6c398fef7bb5c1e33068946cefd84d16cc0c7dfc
PYng-Pong <howman.2018@gmail.com>
2022-11-17 00:40:22 -0300

    tela inicial login teste

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,91 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      class="raquete1"
+      alt="Duas raquetes de tênis de mesa e cinco bolinhas"
+      src="../assets/raquete1.png"
+    />
+    <section class="losango"></section>
+    <section class="login">
+      <h1>Login</h1>
+      <input v-model="user.username" class="input-text" type="text" />
+      <input v-model="user.password" class="input-text" type="password" />
+      <br />
+      <button class="esqueceu-senha">Esqueceu a senha?</button>
+      <br />
+      <button @click="login" class="input-button">Efetuar Login</button>
+    </section>
+    <img
+      class="raquete2"
+      alt="Duas raquetes de tênis de mesa e uma bolinha"
+      src="../assets/raquete2.png"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  data: () => ({
+    user: {},
+  }),
+  methods: {
+    login() {
+      console.log("Usuário:", this.user.username);
+      console.log("Senha:", this.user.password);
+    },
+  },
+};
 </script>
+
+<style>
+.home {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100vh;
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.raquete1 {
+  width: 14em;
+  height: 14em;
+}
+
+.raquete2 {
+  width: 10em;
+  height: 10em;
+  margin: auto 2em;
+}
+
+.losango {
+  height: 14em;
+  width: 14em;
+  background-color: #d9d9d9;
+  transform: rotate(45deg);
+}
+
+.login {
+  height: 13em;
+  width: 18em;
+  background-color: #e9e9e9;
+  position: fixed;
+  left: 34%;
+}
+
+.input-text {
+  border: solid 1px #333;
+  padding: 3px;
+  margin: 3px;
+}
+
+.esqueceu-senha {
+  font-size: 8px;
+}
+
+.input-button {
+  margin-top: 2.3em;
+  border-radius: 3em;
+}
+</style>
