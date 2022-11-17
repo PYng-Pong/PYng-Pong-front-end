b59f3717e4da1a21a7afc9b2d059477b7b77b445
PYng-Pong <howman.2018@gmail.com>
2022-11-17 13:01:48 -0300

    alterar email

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="about">
-    <h1>This is a home page</h1>
+  <div id="home">
+    <section class="columns"></section>
+    <section class="columns">
+      <button class="column button" @click="verPerfil">Ver Perfil</button>
+      <button class="column button">Adicionar um jogador</button>
+      <button class="column button">Remover um jogador</button>
+      <button class="column button">Editar um jogador</button>
+      <button class="column button" @click="paginaJogadores">
+        Ver lista de jogadores
+      </button>
+    </section>
   </div>
 </template>
+
+<script>
+import { mapMutations, mapState } from "vuex";
+export default {
+  methods: {
+    ...mapMutations("auth", ["setHeaders", "unsetHeaders"]),
+
+    verPerfil() {
+      this.$router.push({ path: "/perfil" });
+    },
+    paginaJogadores() {
+      this.$router.push({ path: "/jogadores" });
+    },
+  },
+};
+</script>
+
+<style>
+#home {
+  display: flex;
+  justify-content: space-between;
+  height: 85vh;
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.columns {
+  flex-direction: column;
+  margin: 2em;
+}
+
+.column {
+  background-color: #d9d9d9;
+  padding: 1.2em;
+  margin: 4px;
+}
+</style>
--- src/views/PerfilView.vue
+++ src/views/PerfilView.vue
@@ -1,5 +1,83 @@
 <template>
-  <div class="about">
-    <h1>This is a profile page</h1>
+  <div id="pefil">
+    <section class="forms">
+      <div class="field">
+        <label class="label">Nome</label>
+        <div class="control">
+          <input
+            disabled
+            class="input"
+            type="text"
+            v-model="newUser.username"
+            placeholder="e.g Alex Smith"
+          />
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+          <input
+            class="input"
+            type="email"
+            v-model="newUser.email"
+            placeholder="e.g. alexsmith@gmail.com"
+          />
+        </div>
+      </div>
+      <div class="control">
+        <button class="button is-primary" @click="updateInfo">
+          Atualizar dados
+        </button>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+export default {
+  created() {
+    this.newUser = { ...this.user };
+  },
+  data: () => ({
+    newUser: null,
+  }),
+  computed: {
+    ...mapState("auth", ["user"]),
+  },
+  methods: {
+    ...mapActions("auth", ["updateUser"]),
+
+    async updateInfo() {
+      if (this.newUser.email != this.user.email) {
+        try {
+          await this.updateUser(this.newUser);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#perfil {
+  display: flex;
+  height: 100vh;
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.forms {
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 2em;
+}
+
+.control {
+  margin-top: 1.3em;
+}
+</style>
